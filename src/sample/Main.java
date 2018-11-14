a7aff15ac5b9f7350935dd25b83d8e4eacb8c2e5
sesseladdo <vanaddo@gmail.com>
2018-11-14 15:22:12 -0500

    Tutorial #3: Creating and Switching between Multiple Sessions.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,46 +5,51 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-public class Main extends Application implements EventHandler<ActionEvent>{
+public class Main extends Application {
 
-    Button button;
+  Stage window;
+  Scene scene1, scene2;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
+  @Override
 
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Title of the Window");
-        button = new Button();
-        button.setText("Click me");
+  public void start(Stage primaryStage) throws Exception {
 
-       //This class will handle the button events
-      button.setOnAction(e -> {
-      System.out.println("Hey brown cow ");
-        System.out.println("I am a meatball");
-      });
+    window = primaryStage;
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+    Label label1 = new Label("Welcome to the first scene!");
+    Button  button1 = new Button("Go to scene 2");
+    button1.setOnAction(e -> window.setScene((scene2)));
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    //Layout1- children are laid out in vertical columns
+    VBox layout1 = new VBox(20);
+    layout1.getChildren().addAll(label1,button1);
+    scene1 = new Scene(layout1, 299,300);
 
-    @Override
-    public void handle(ActionEvent event) {
-        //handle is an event called whenever the user clicks the button. Essentially what they want to do
+    //Button2
+    Button button2 = new Button("I'm so sleepy");
+    button2.setOnAction(e -> window.setScene(scene1));
 
-        if (event.getSource()== button){
-            System.out.println("You clicked the button!");
-        }
+    //Layout2
+    StackPane layout2 = new StackPane();
+    layout2.getChildren().add(button2);
+    scene2 = new Scene(layout2, 600,300);
 
-    }
+    window.setScene(scene1);
+    window.setTitle("Title here");
+    window.show();
+
+  }
+
+
 }
+
