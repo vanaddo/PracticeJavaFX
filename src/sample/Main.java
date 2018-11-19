94b867a36c7c9cccb4ab459d4d1c3c6d4bf0603d
sesseladdo <vanaddo@gmail.com>
2018-11-19 15:21:19 -0500

    Tutorial #11: CheckBox
    *How and when to create new checkbox.
    *How set a default check in a check box .setSelected(true).
    *How to see if a checkbox has been selected .isSelected().

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -27,15 +28,26 @@ public class Main extends Application {
   public void start(Stage primaryStage){
 
     window = primaryStage;
-    window.setTitle("JavaFx- The New Boston Tutorial");
-   //Form
-    TextField nameInput = new TextField("Sam");
-    button = new Button("Click Me");
-    button.setOnAction(e -> isInt(nameInput,nameInput.getText()));
+    window.setTitle("JavaFx- CheckBox Subs");
+//   //Form
+//    TextField nameInput = new TextField("Sam");
+
+
+    //Checkboxes
+
+    CheckBox box1 = new CheckBox("Bacon");
+    CheckBox box2 = new CheckBox("Salami");
+    box2.setSelected(true);
+    //Button
+    button = new Button("Order Now!");
+    button.setOnAction(e -> handleOptions(box1, box2));
+//  button.setOnAction(e -> isInt(nameInput,nameInput.getText()));
+//
+;
   //Layout
     VBox layout = new VBox(10);
     layout.setPadding(new Insets(20,20,20,20));
-    layout.getChildren().addAll(nameInput, button);
+    layout.getChildren().addAll(box1,box2, button);
 
     scene = new Scene(layout,300,250);
 
@@ -78,16 +90,27 @@ public class Main extends Application {
     window.show();
 
   }
-
-  private boolean isInt(TextField input, String message){
-    try {
-      int age = Integer.parseInt(input.getText());
-      System.out.println("User is: " +age);
-      return true;
-    }catch (NumberFormatException e){
-      System.out.println("Error: " + message + " is not a number");
-      return false;
+//Handle Check Box Options
+  private void  handleOptions(CheckBox box1,CheckBox box2) {
+    String message = "You ordered:\n";
+    if(box1.isSelected()){
+      message += "Bacon\n";
+  }
+    if (box2.isSelected()){
+      message += "Tuna\n";
     }
+    System.out.println(message);
+
+
+//  private boolean isInt(TextField input, String message){
+//    try {
+//      int age = Integer.parseInt(input.getText());
+//      System.out.println("User is: " +age);
+//      return true;
+//    }catch (NumberFormatException e){
+//      System.out.println("Error: " + message + " is not a number");
+//      return false;
+//    }
   }
 
 
