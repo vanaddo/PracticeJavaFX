87b0cef2859f09935d936c5df952dadbf302406b
Sam <vanaddo@gmail.com>
2018-11-27 15:20:33 -0500

    Tutorial #18: Simple TableView
    **Showed how to set up a table in JavaFx and pull in objects or values from an observable list into the newly created table.
    
    **Create a new table column and gives it a header title:
     TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
    
     **Sets column dimensions:
      nameColumn.setWidth(200); //not setting with causes columns to bunch up
    
      **Sets inputs values into the column cells:
       name.Column.setCellValueFactory(new PropertyValueFactory<>("name")); //("name")points to the property values that you want pulled into specified column

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,25 @@
 package sample;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import sun.reflect.generics.tree.Tree;
+
 
 
 public class Main extends Application {
 
   Stage window;
-   Scene scene;
-//  Button button;
-//  ComboBox<String> comboBox;
-//  ListView<String>listView;
+  TableView<Product>table;
 
-  TreeView<String> tree;
 
 
   public static void main(String[] args) {
@@ -25,197 +27,50 @@ public class Main extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage){
+  public void start(Stage primaryStage) throws Exception {
 
     window = primaryStage;
-    window.setTitle("JavaFx- ChoiceBox");
+    window.setTitle("thenewboston-JavaFx");
 
-    TreeItem<String> root,sam,paul;
+    //Name Column
+    TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+    nameColumn.setMinWidth(200);
+    nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-    //Root
+    //Price Column
+    TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+    priceColumn.setMinWidth(100);
+    priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-    root = new TreeItem<>();
-    root.setExpanded(true);
+    //Quantity Column
+    TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
+    quantityColumn.setMinWidth(100);
+    quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
-    //sam Branch
-    sam = makeBranch("Sam", root);
-    makeBranch("Thai Food", sam);
-    makeBranch("Chocolate", sam);
-    makeBranch("Warm Weather", sam);
+    //Set Table Up
+    table = new TableView<>();
+    table.setItems(getProduct());
+    table.getColumns().addAll(nameColumn,priceColumn,quantityColumn);
 
-    //Paul Branch
-    paul = makeBranch("Paul", root);
-    makeBranch("Italian Food",paul);
-    makeBranch("Ice Cream",paul);
-    makeBranch("Fall Weather", paul);
+    VBox vBox = new VBox();
+    vBox.getChildren().addAll(table);
 
-  //create tree
-    tree = new TreeView<>(root);
-    tree.setShowRoot(false);
-    tree.getSelectionModel().selectedItemProperty()
-            .addListener((v, oldValue, newValue)->{
-              if (newValue != null)
-                System.out.println(newValue.getValue());
-            });
-
-    /*button = new Button("Submit");
-
-    listView = new ListView<>();
-    listView.getItems().addAll("Black Panther","Brown Sugar","Boomerang","Blank Man!");
-    listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-    button.setOnAction(e-> buttonClicked());*/
-   /* comboBox = new ComboBox<>();
-    comboBox.getItems().addAll(
-            "Love and Basketball",
-            "Black Panther",
-            "Harlem Nights"
-    );
-
-    comboBox.setPromptText("What is your favorite movie?");
-
-    comboBox.setEditable(true);
-    button.setOnAction(e -> printMovie());
-    comboBox.setOnAction(e-> System.out.println("User selected: " + comboBox.getValue()));*/
-
-//   //Form
-//    TextField nameInput = new TextField("Sam");
-
-
-//    //Checkboxes
-//
-//    CheckBox box1 = new CheckBox("Bacon");
-//    CheckBox box2 = new CheckBox("Salami");
-//    box2.setSelected(true);
-
-    //Button
-
-//    button.setOnAction(e -> handleOptions(box1, box2));
-//  button.setOnAction(e -> isInt(nameInput,nameInput.getText()));
-//
-
-//    //ChoiceBox
-//
-//    ChoiceBox<String> choiceBox = new ChoiceBox<>();
-//
-//    //getItems returns theObservableList object which you can add items to
-//    choiceBox.getItems().add("Apples");
-//    choiceBox.getItems().add("Oranges");
-//    choiceBox.getItems().add("Grapes");
-//    choiceBox.getItems().add("Cherries");
-//    choiceBox.getItems().addAll("Bananas","Strawberries","Grapefruits");
-//    choiceBox.setValue("Apples");
-//
-//   //Listen for selection changes
-//    choiceBox.getSelectionModel().selectedItemProperty().addListener((v,oldValue, newValue) -> System.out.println(newValue));
-   //button.setOnAction(e-> getChoice(choiceBox));
-
-  //Layout
-    VBox layout = new VBox(10);
-    layout.setPadding(new Insets(20,20,20,20));
-    layout.getChildren().add(tree);
-
-    scene = new Scene(layout,300,250);
-
-
-    /*//Name label
-    Label nameLabel = new Label("Username:");
-    GridPane.setConstraints(nameLabel,0,0);
-
-    //Name input
-    TextField nameInput = new TextField("Sam");
-    GridPane.setConstraints(nameInput,1,0);
-
-    //Password Label
-    Label passLabel = new Label("Password");
-    GridPane.setConstraints(passLabel,0,1);
-
-    //Password Input
-
-    TextField passInput = new TextField();
-    passInput.setPromptText("password");
-    GridPane.setConstraints(passInput,1,1);
-
-    Button loginButton = new Button("Log In");
-    GridPane.setConstraints(loginButton,1,2);
-
-    grid.getChildren().addAll(nameLabel,nameInput,passLabel,passInput,loginButton);
-    Scene scene = new Scene(grid,300,200);*/
-
+    Scene scene = new Scene(vBox);
     window.setScene(scene);
     window.show();
+  }
 
-
-
-
-
-
-   /* StackPane layout = new StackPane();
-    layout.getChildren().add(button);
-    Scene scene = new Scene(layout,300,250);
-    window.setScene(scene);
-    window.show();
-    */
+  //Gets all of the products
+  public ObservableList<Product> getProduct(){
+    //points the system to the database or file that it needs to get the info from
+   ObservableList<Product>products = FXCollections.observableArrayList();
+   products.add(new Product("Laptop",350.00,1));
+   products.add(new Product("IPad",300.00,1));
+   products.add(new Product("Headphones",180.00,1));
+   products.add(new Product("Monitor",150.00,2));
+   products.add(new Product("Extension Cords",10.00,2));
+   return products;
 
   }
-//Create Branches
-
-  public TreeItem<String> makeBranch(String title, TreeItem<String> parent){
-    TreeItem<String> item = new TreeItem<>(title);
-    item.setExpanded(true);
-    parent.getChildren().add(item);
-    return item;
-  }
-/*
-  private void buttonClicked(){
-    java.lang.String message = "";
-    ObservableList<String> movies;
-    movies = listView.getSelectionModel().getSelectedItems();
-
-    for (String m: movies){
-      message += m +"\n";
-    }
-    System.out.println(message);
-  }*/
- /* //Print out a movie
-  private void printMovie(){
-    System.out.println(comboBox.getValue());
-  }*/
- /*//Handle Check Box Options
-  private void  handleOptions(CheckBox box1,CheckBox box2) {
-    String message = "You ordered:\n";
-    if (box1.isSelected()) {
-      message += "Bacon\n";
-    }
-    if (box2.isSelected()) {
-      message += "Tuna\n";
-    }
-    System.out.println(message);
-  }*/
-
-
-/* private void getChoice( ChoiceBox<String> choiceBox){
-   String food = choiceBox.getValue();
-   System.out.println(food);
- }*/
-//  private boolean isInt(TextField input, String message){
-//    try {
-//      int age = Integer.parseInt(input.getText());
-//      System.out.println("User is: " +age);
-//      return true;
-//    }catch (NumberFormatException e){
-//      System.out.println("Error: " + message + " is not a number");
-//      return false;
-//    }
-
-
-
-  /*private void closeProgram(){
-   Boolean answer = ConfirmBox.display("Title" , "Are you sure you want to exit the program?");
-   if (answer)
-      window.close();
-  }
-*/
-
 }
 
