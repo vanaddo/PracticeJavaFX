4fe572e5da5f4ec3601cdc25e6c5e4f7e194d4e9
sesseladdo <Millie63>
2018-11-29 11:31:01 -0500

    Tutorial #19: Editable Tables
    **Created Text input fields allowing users to add items to the existing table.
    **Worked more on the aesthetics and appearance in this tutorial rather than functionality.
     -TextField();
     -.setPromptText() : prompts user on the type of info needed in field i.e "password"
     -.setMinWidth() : sets the min width of the table columns or input field
    
    Tutorial #20: Adding & Deleting TableView Rows
    **Made the input fields, add & delete button functional.
    **Parsed the inputted fields into the correct Java numerical types using a NumberFormatException to throw an exception when the input entered doesn't contain a parsable numerical type.
    
    Ex:
     product.setPrice(Double.parseDouble(priceInput.getText()));

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 
   Stage window;
   TableView<Product>table;
+  TextField nameInput, priceInput, quantityInput;
 
 
 
@@ -47,28 +48,85 @@ public class Main extends Application {
     quantityColumn.setMinWidth(100);
     quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
+    //Name Input
+    nameInput = new TextField();
+    nameInput.setPromptText("Name");
+    nameInput.setMinWidth(100);
+
+    //Price Input
+    priceInput = new TextField();
+    priceInput.setPromptText("Price");
+
+
+    //Quantity Input
+    quantityInput = new TextField();
+    quantityInput.setPromptText("Quantity");
+
+    //Add & Delete Buttons
+    Button addButton = new Button("Add");
+    addButton.setOnAction(e-> addButtonClicked());
+    Button deleteButton = new Button("Delete");
+    deleteButton.setOnAction(e-> deleteButtonClicked());
+
+  //setting our variables into HBoc=x sets their layout to be set up horizontally
+    HBox hBox = new HBox();
+    hBox.setPadding(new Insets(10,10,10,10));
+    hBox.setSpacing(10); //creates spacing between the "add" & "delete" buttons, and inputs
+    hBox.getChildren().addAll(nameInput,priceInput,quantityInput,addButton,deleteButton);
+
+
+
+
+
+
+
     //Set Table Up
     table = new TableView<>();
     table.setItems(getProduct());
     table.getColumns().addAll(nameColumn,priceColumn,quantityColumn);
 
     VBox vBox = new VBox();
-    vBox.getChildren().addAll(table);
+    vBox.getChildren().addAll(table,hBox); // placing the Hbox in here allows the variables listed in the HBox settings to show on the table itself
 
     Scene scene = new Scene(vBox);
     window.setScene(scene);
     window.show();
   }
 
+  //Add Button Clicked
+  public void addButtonClicked(){
+    Product product = new Product();
+    product.setName(nameInput.getText());
+    product.setPrice(Double.parseDouble(priceInput.getText())); //Look at your notes to reference NumberFormatException
+    product.setQuantity(Integer.parseInt(quantityInput.getText()));
+    table.getItems().add(product); //adds the inputted product info to the table
+    nameInput.clear(); //clears the inputted fields after the product has been added to the table
+    priceInput.clear();
+    quantityInput.clear();
+  }
+
+//Delete Button Clicked
+
+  public void deleteButtonClicked(){
+    ObservableList<Product> productSelected, allProducts; // creates two observable list. The 1st is the product that the user selected the 2nd is a list of all the existing products in the table
+    allProducts = table.getItems(); //gets all the existing products from the table
+    productSelected = table.getSelectionModel().getSelectedItems(); //returns any item that the user has selected(Clicked on)
+
+    productSelected.forEach(allProducts::remove); // for each product that the user has selected remove from the all products list(Main Table)
+
+
+
+  }
+
   //Gets all of the products
   public ObservableList<Product> getProduct(){
     //points the system to the database or file that it needs to get the info from
    ObservableList<Product>products = FXCollections.observableArrayList();
-   products.add(new Product("Laptop",350.00,1));
-   products.add(new Product("IPad",300.00,1));
-   products.add(new Product("Headphones",180.00,1));
-   products.add(new Product("Monitor",150.00,2));
-   products.add(new Product("Extension Cords",10.00,2));
+   products.add(new Product("Laptop",350.00,30));
+   products.add(new Product("IPad",300.00,25));
+   products.add(new Product("Headphones",180.00,15));
+   products.add(new Product("Monitor",150.00,40));
+   products.add(new Product("Extension Cords",10.00,200));
    return products;
 
   }
